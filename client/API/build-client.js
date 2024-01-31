fe21826936882d06dce15fc04607bdb6587018f6
Paweł Łuszkiewicz <matryca@gmail.com>
2024-01-31 18:35:24 +0100

    preparing digitalocean upload

--- client/API/build-client.js
+++ client/API/build-client.js
@@ -5,8 +5,7 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.capsule-corp.pl',
       headers: req.headers,
     });
   } else {
